9346fd1b4fcc0fea0fcaa94d200e3f3508000056
jackcornblum <jcornblum@gmail.com>
2021-05-18 16:29:31 -0400

    commit tree 23d1043a8819c13afb1fbe78414866fab060dba4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+myArray = ['Sally', 'Bob', 'John', 'Derek'];
+
+
+const returnFirstTwoDrivers = function(array) {
+    let newArray = [...array.splice(0,2)];
+    return newArray;
+}
+
+const returnLastTwoDrivers = function(array) {
+    let newArray = [...array.splice(array.length-2,array.length-1)];
+    return newArray;
+}
+
